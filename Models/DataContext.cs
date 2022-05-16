c232c3862d4f6520196214c091f1fb2d9d96a5fd
KevinPa26 <kevinenriquep26@gmail.com>
2022-05-16 20:43:40 -0300

    Initial commit

---
+++ Models/DataContext.cs
@@ -0,0 +1,17 @@
+using InmobiliariaApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inmobiliaria_.Net_Core.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Propietario> propietario { get; set; }
+    }
+}
